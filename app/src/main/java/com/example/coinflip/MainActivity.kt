7ec725344db787fca270488a0954c1df093f7b1d
HiImZsofi <zsofia.gyori00@gmail.com>
2022-10-14 13:21:55 +0200

    add game over alert dialog

--- app/src/main/java/com/example/coinflip/MainActivity.kt
+++ app/src/main/java/com/example/coinflip/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.coinflip
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rlayout : RelativeLayout
@@ -16,6 +17,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var wins : TextView
     private lateinit var losses : TextView
 
+    var isHead : Boolean = false
+    var isTail : Boolean = false
+    var throws : Int = 0
+    var win : Int = 0
+    var lose : Int = 0
+    var rnd : Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,50 +31,57 @@ class MainActivity : AppCompatActivity() {
 
         init()
 
-        var isHead : Boolean = false
-        var isTail : Boolean = false
-        var throws : Int = 0
-        var win : Int = 0
-        var lose : Int = 0
-        var rnd : Int = 0
-        guessHead.setOnClickListener{
-            isHead = true
-            throws++
-            rnd = (1..2).random()
-            if(rnd == 1){
-                win++
-                image.setImageResource(R.drawable.heads)
-                guesses.setText("Dobások: " + throws.toString())
-                Toast.makeText(this@MainActivity, "A dobás értéke fej!", Toast.LENGTH_SHORT).show()
-                wins.setText("Győzelmek: " + win.toString())
-            }else{
-                lose++
-                image.setImageResource(R.drawable.tails)
-                guesses.setText("Dobások: " + throws.toString())
-                Toast.makeText(this@MainActivity, "A dobás értéke írás!", Toast.LENGTH_SHORT).show()
-                losses.setText("Vereségek: " + lose.toString())
-            }
-        }
 
-        guessTail.setOnClickListener{
-            isTail = true
-            throws++
-            rnd = (1..2).random()
-            if(rnd == 2){
-                win++
-                image.setImageResource(R.drawable.heads)
-                guesses.setText("Dobások: " + throws.toString())
-                Toast.makeText(this@MainActivity, "A dobás értéke írás!", Toast.LENGTH_SHORT).show()
-                wins.setText("Győzelmek: " + win.toString())
-            }else{
-                lose++
-                image.setImageResource(R.drawable.tails)
-                guesses.setText("Dobások: " + throws.toString())
-                Toast.makeText(this@MainActivity, "A dobás értéke fej!", Toast.LENGTH_SHORT).show()
-                losses.setText("Vereségek: " + lose.toString())
-            }
-        }
 
+            guessHead.setOnClickListener{
+                isHead = true
+                throws++
+                println(throws)
+                rnd = (1..2).random()
+                if(throws >= 5){
+                    startOver()
+                }
+                else{
+                    if(rnd == 1){
+                        win++
+                        image.setImageResource(R.drawable.heads)
+                        guesses.setText("Dobások: " + throws.toString())
+                        Toast.makeText(this@MainActivity, "A dobás értéke fej!", Toast.LENGTH_SHORT).show()
+                        wins.setText("Győzelmek: " + win.toString())
+                    }else if(rnd == 2){
+                        lose++
+                        image.setImageResource(R.drawable.tails)
+                        guesses.setText("Dobások: " + throws.toString())
+                        Toast.makeText(this@MainActivity, "A dobás értéke írás!", Toast.LENGTH_SHORT).show()
+                        losses.setText("Vereségek: " + lose.toString())
+                    }
+                }
+            }
+
+            guessTail.setOnClickListener{
+                isTail = true
+                throws++
+                println(throws)
+                rnd = (1..2).random()
+                if(throws >= 5){
+                    startOver()
+                }
+                else{
+                    if(rnd == 2){
+                        win++
+                        image.setImageResource(R.drawable.tails)
+                        guesses.setText("Dobások: " + throws.toString())
+                        Toast.makeText(this@MainActivity, "A dobás értéke írás!", Toast.LENGTH_SHORT).show()
+                        wins.setText("Győzelmek: " + win.toString())
+                    }else if(rnd == 1){
+                        lose++
+                        image.setImageResource(R.drawable.heads)
+                        guesses.setText("Dobások: " + throws.toString())
+                        Toast.makeText(this@MainActivity, "A dobás értéke fej!", Toast.LENGTH_SHORT).show()
+                        losses.setText("Vereségek: " + lose.toString())
+                    }
+                }
+            }
     }
 
     fun init(){
@@ -80,5 +94,45 @@ class MainActivity : AppCompatActivity() {
         guesses = findViewById(R.id.guesses)
         wins = findViewById(R.id.wins)
         losses = findViewById(R.id.losses)
+    }
+
+    fun startOver() {
+        var gameOverDialogBuilder = AlertDialog.Builder(this@MainActivity)
+        if(win > lose){
+            gameOverDialogBuilder.setTitle("Győzelem!")
+            gameOverDialogBuilder.setMessage("Szeretne új játékot játszani?")
+            gameOverDialogBuilder.setCancelable(false)
+            gameOverDialogBuilder.setNegativeButton("Nem", DialogInterface.OnClickListener {
+                    dialog, id -> finish()
+            })
+            gameOverDialogBuilder.setPositiveButton("Igen", DialogInterface.OnClickListener {
+                    dialog, id -> win = 0
+                lose = 0
+                throws = 0
+                guesses.setText("Dobások: " + throws.toString())
+                losses.setText("Vereségek: " + lose.toString())
+                wins.setText("Győzelmek: " + win.toString())
+                image.setImageResource(R.drawable.heads)
+            })
+            gameOverDialogBuilder.show()
+        }
+        else if(lose > win){
+            gameOverDialogBuilder.setTitle("Vereség!")
+            gameOverDialogBuilder.setMessage("Szeretne új játékot játszani?")
+            gameOverDialogBuilder.setCancelable(false)
+            gameOverDialogBuilder.setNegativeButton("Nem", DialogInterface.OnClickListener {
+                    dialog, id -> finish()
+            })
+            gameOverDialogBuilder.setPositiveButton("Igen", DialogInterface.OnClickListener {
+                    dialog, id -> win = 0
+                lose = 0
+                throws = 0
+                guesses.setText("Dobások: " + throws.toString())
+                losses.setText("Vereségek: " + lose.toString())
+                wins.setText("Győzelmek: " + win.toString())
+                image.setImageResource(R.drawable.heads)
+            })
+            gameOverDialogBuilder.show()
+        }
     }
 }
